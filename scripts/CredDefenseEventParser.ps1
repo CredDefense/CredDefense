d7a272b0b0d654d7cec35b510bfd4e2779e3217d
dafthack <beau@dafthack.com>
2017-09-18 15:08:46 -0700

    Generated Base for CredDefenseEventParser
    CredDefenseEventParser Script outline

---
+++ scripts/CredDefenseEventParser.ps1
@@ -0,0 +1,63 @@
+﻿function Invoke-CredDefenseEventParser{
+    
+    Param(
+
+    [Parameter(Position = 0, Mandatory = $false)]
+    [string]
+    $EventLog = ""
+
+    )
+
+    #Start loop to ingest logs
+
+    #If a single event log is set process it. Otherwise process the forwarded events.
+    if ($EventLog -ne "")
+    {
+        $filter = @{
+            Path = "$EventLog"
+            #StartTime = (Get-Date)AddHours(-1)
+            #EndTime = Get-Date
+        }
+    }
+    else
+    {
+        $filter = @{
+            Path = "$env:SystemRoot\System32\Winevt\Logs\ForwardedEvents.evtx"
+            #StartTime = (Get-Date)AddHours(-1)
+            #EndTime = Get-Date
+        }
+    }
+
+    $events = Get-WinEvent -FilterHashtable $filter
+
+    ####### Start various alert triggers #######
+    ### Section for detecting Password Spraying
+
+
+    ### Section for detecting Honey Token Usage
+
+
+    ### Section for detecting Responder activity
+
+
+    ####### Logging and Alerting ######
+   
+    #Write CredDefense log file if one doesn't exist
+    $CDLogPath = 'C:\logs\'
+    $CDLogFileName = 'cdlog.log'
+    $CDLog = $CDLogPath+$CDLogFileName
+    if (Test-Path $CDLog)
+    {
+        $loglastwrite = (Get-ChildItem $CDLog).LastWriteTime 
+    }
+    else
+    {
+        mkdir $CDLogPath
+        echo $null > $CDLog
+    }
+
+    #checks CredDefense log file to see if an alert already existed
+
+    #On screen alerting & email notification
+
+}
